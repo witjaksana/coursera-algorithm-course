467e1f6d32f38e234cd066a1f17ade66228d29cb
Arief WICAKSANA <Arief@Ariefs-MacBook-Pro.local>
2020-05-17 15:42:37 +0200

    init files

---
+++ percolation/Percolation.java
@@ -0,0 +1,16 @@
+/* *****************************************************************************
+ *  Name: Arief
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class Percolation {
+
+    public Percolation(int n) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
